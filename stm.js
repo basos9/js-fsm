85acc944a13a6a14d6a4bb3928e0fac4f0855d1c
basos9 <basos.g 4t gmx d0t net>
2014-03-28 16:07:22 +0200

    RFCT no mod original, accept GLOB keys

--- stm.js
+++ stm.js
@@ -32,6 +32,8 @@
     }
   };
 
+  StateMachine.GLOB =  '__STMGLOB__';
+
    /**
    * isArray shim
    * @param value mixed
@@ -56,49 +58,95 @@
       return new Func();
     };
 
+  /**
+   * Object.keys() shim
+   * Get an array of an Object's keys
+   */
+  StateMachine.keys = typeof Object.keys !== 'undefined' ? Object.keys :
+    function (o) {
+      var i,
+        keys = [],
+        hasOwnProperty = Object.prototype.hasOwnProperty;
+
+      if (typeof o !== 'object' && typeof o !== 'function' || o === null) throw new TypeError('Object.keys called on non-object');
+      for(i in o) {
+        if (hasOwnProperty.call(o,i)) keys.push(i);
+      }
+      return keys;
+    }; // end function Object.keys
+
   StateMachine.prototype = {
-    stmInit: function (states, log) {
-      var initial, i, e, j, f, k, z, n,
+
+    stmInit: function (statesDef, log) {
+      var initial, i, e, j, f, k, z, n, t,
+        // Create new states object (we will mutate some things)
+        states = this.states = {},
         c = 0;
+
+      function ammendCondCont(f) {
+        var z = f.cond,
+          GLOB = StateMachine.GLOB,
+          globKeys = GLOB in z,
+          d, i;
+        if (globKeys) {
+          // copy object, skip glob
+           d = z;
+           f.cond = z = {};
+           for (i in d)
+             if (i !== GLOB)
+               z[i] = d[i];
+           // f.cond = z = StateMachine.create(z); //clone
+           // z[StateMachine.GLOB] = undefined; // mask glob
+        }
+        f._stmCondLen = StateMachine.keys(z).length;
+        f._stmCondGlobKeys = globKeys;
+      }
+
       this.log = log || this.log || function(){};
 
-      this.states = states;
-
-      for (i in states) {
+      for (i in statesDef) {
           if (i === 'initial') {
-            initial = states[i];
+            initial = statesDef[i];
             continue;
           }
-          e = states[i]; // state def (array of transitions)
-          if (!e._events) e._events = {};
-          e._name = i;
+          e = statesDef[i];
+          t = states[i] = []; // state def (array of transitions)
+          t._stmEvents = {}; // events prop
+          t._stmName = i; // name prop
           for (j=e.length; j--; ){
             f = e[j];
             if (!f.to) throw new Error("Missing 'to' member");
-            if (!states[f.to]) throw new Error("State not defined "+f.to);
+            if (!statesDef[f.to]) throw new Error("State not defined "+f.to);
             this._stmAction(f.action, true); // dry run
             if ( (z = f.event)) {
               // event transition from this state
               if (StateMachine.isArray(z)) {
                 for (k=z.length; k--; ) {
-                  e._events[z[k]] = f;
+                  t._stmEvents[z[k]] = f;
                 }
               }
               else {
-                e._events[z] = f;
+                t._stmEvents[z] = f;
               }
-              e.splice(j, 1); // splice ok on reverse read array
+              //DONT MODIFY ORIGINAL e.splice(j, 1); // splice ok on reverse read array
             }
             else if ((z = f.cond)) {
               if (StateMachine.isArray(z)) {
                 for (k=z.length; k--; ) {
                   n = StateMachine.create(f); // clone proto
                   n.cond = z[k]; // mask cond
-                  e.push(n); // append ok on reverse read array
+                  ammendCondCont(n);
+                  //DONT MOD ORIGINAL e.push(n); //  append ok on reverse read array
+                  t.push(n);
                 }
                 // delete ref to original cond array
                 // member objects of this array are still referenced by n objects above with cond property masked
-                e.splice(j, 1); // splice ok on reverse read array
+                // DONT MOD ORIGINAL e.splice(j, 1); // splice ok on reverse read array
+              }
+              else {
+                n = StateMachine.create(f);
+                ammendCondCont(f);
+                t.push(f);
               }
             }
           }
@@ -106,7 +154,7 @@
       }
 
       if (! initial || ! initial in states)
-        throw new Error("initial state ("+initial+")not defined");
+        throw new Error("initial state ("+initial+") not defined");
 
       this.currentState = states[initial];
 
@@ -137,19 +185,20 @@
     stmOnEvent: function(e){
       var state = this.currentState,
         f;
-      if( (f = state._events[e])){
-        this.log("stmOnEvent() > switching to: "+f.to+", e: "+e+", from: "+state._name+", action: "+!!f.action);
+      if( (f = state._stmEvents[e])){
+        this.log("stmOnEvent() > switching to: "+f.to+", e: "+e+", from: "+state._stmName+", action: "+!!f.action);
         this.currentState = this.states[f.to] ;
         this._stmAction(f.action);
       }
       else {
-        this.log("stmOnEvent() no switch evt e: "+e+", from: "+state._name);
+        this.log("stmOnEvent() no switch evt e: "+e+", from: "+state._stmName);
       }
 
     },
 
     stmOnCondition: function(d){
       var state = this.currentState,
+          GLOB = StateMachine.GLOB,
           j, f, k, z, has;
        if (d !== null && typeof d === 'object')
          for (j=state.length; j--; ){
@@ -158,11 +207,15 @@
              has = true;
              for (k in z) {
                if (!(has = has &&
-                 (k in d && (z[k] === '*' || z[k] === d[k]))))
+                 (k in d && (z[k] === GLOB || z[k] === d[k]))))
                  break;
              }
+             // if matched and globKeys not specifies, lengths should match (since all condSpec attrs matched
+             //  if lengths also match (minus glob in condspec) this means that both objects have the same attrs
+             has = has && ( f._stmCondGlobKeys ||
+                StateMachine.keys(d).length === f._stmCondLen);
              if (has) {
-               this.log("stmOnCondition() > switching to: "+f.to+", d: "+(typeof JSON !== 'undefined' && JSON.stringify(d))+", from: "+state._name+", action: "+!!f.action);
+               this.log("stmOnCondition() > switching to: "+f.to+", d: "+(typeof JSON !== 'undefined' && JSON.stringify(d))+", from: "+state._stmName+", action: "+!!f.action);
                this.currentState = this.states[f.to];
                this._stmAction(f.action);
                break;
@@ -170,12 +223,12 @@
            }
          }
        if (!has) {
-         this.log("stmOnCondition() no switch cond d: "+(typeof JSON !== 'undefined' && JSON.stringify(d))+", from: "+state._name);
+         this.log("stmOnCondition() no switch cond d: "+(typeof JSON !== 'undefined' && JSON.stringify(d))+", from: "+state._stmName);
        }
     },
 
     stmGetStatus: function(){
-      return this.currentState._name;
+      return this.currentState._stmName;
     }
   };
 
